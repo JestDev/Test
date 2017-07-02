6b7dec065b29956d4ff64bcb4955961657ecd030
Vladislav <JestemDevOfficial@gmail.com>
2017-07-02 19:18:22 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Auto {
+private:
+   int year;
+   int month;
+   int day;
+public:
+   void set (int date_year, int date_month, int date_day) {
+      year = date_year;
+      month = date_month;
+      day = date_day;
+   }
+   void get () {
+      cout << "This car made on " << year << "." << month << "." << day << endl;
+   }
+};
+
+int main() {
+   Auto Lada;
+   Lada.set(2003, 5, 18);
+   Lada.get();
+   
+   cin.get();
+   return 0;
+}
