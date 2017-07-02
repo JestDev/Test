dbb55a705cc3fb63afd28363836f25bb7355639e
Vladislav <JestemDevOfficial@gmail.com>
2017-07-02 21:11:44 +0300

    Main body of repository

--- main.cpp
+++ main.cpp
@@ -1,33 +1,22 @@
 #include <iostream>
 #include <string>
+#include "Classes.h"
 
 using namespace std;
 
-class Auto {
-private:
-   int year;
-   int month;
-   int day;
-public:
-   void set (int date_year, int date_month, int date_day) {
-      year = date_year;
-      month = date_month;
-      day = date_day;
-   }
-   void get () {
-      cout << "This car made on " << year << "." << month << "." << day << endl;
-   }
-};
-
 int main() {
-   Auto Lada;
-   Lada.set(2003, 5, 18);
-   Lada.get();
-   
-   Auto Shkoda;
-   Shkoda.set(2010, 3, 16);
-   Shkoda.get();
-   
+   AutoData Skoda;
+   Skoda.set_date();
+   Skoda.get_date();
+   Skoda.set_color();
+   Skoda.get_color();
+   Skoda.set_wheels();
+   Skoda.get_wheels();
+   Skoda.set_doors();
+   Skoda.get_doors();
+   Skoda.data();
+
+
    cin.get();
    return 0;
 }
